e8bc06569e685edc4d2e62b1753f612ab5dde16a
jina <jina.oh.dev@gmail.com>
2022-09-05 15:29:46 +0900

    1436

---
+++ java/src/step1/#Q2588.java
@@ -0,0 +1,10 @@
+package step1;
+import java.util.Scanner;
+
+public class Q2588 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
--- java/src/step1/Q18108.java
+++ java/src/step1/Q18108.java
@@ -6,5 +6,6 @@ public class Q18108 {
 public static void main(String[] args){
     Scanner sc =new Scanner(System.in);
     System.out.println(sc.nextInt()-543);
+    sc.close();
 }
 }
