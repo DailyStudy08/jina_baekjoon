bfe4ecde7d152237897f78a3f0f2cc565846b581
jina <jina.oh.dev@gmail.com>
2022-10-02 23:19:33 +0900

    commit

---
+++ java/step06_문자열/Q2941.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q2941 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String[] str = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        String s = sc.next();
+        for (int i = 0; i < str.length; i++) {
+            if(s.contains(str[i])){
+                s=s.replaceAll(str[i], "a");
+            }
+        }
+        System.out.println(s.length());
+
+    }
+}
