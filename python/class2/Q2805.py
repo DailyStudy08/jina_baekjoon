de41da121e63b12af18dfbbc141892646a2ef7c4
jina <jina.oh.dev@gmail.com>
2022-10-13 22:58:46 +0900

    2805

---
+++ python/class2/Q2805.py
@@ -0,0 +1,16 @@
+import sys
+n,m =map(int,sys.stdin.readline().split())
+trees=list(map(int,sys.stdin.readline().split()))
+right = max(trees)
+left = 0
+while left < right :
+    mid = (left + right ) //2
+    total = 0
+    for tree in trees:
+        if tree > mid:
+            total+= (tree-mid)
+    if total > m :
+        left = mid +1
+    else :
+        right = mid-1
+print(right)
