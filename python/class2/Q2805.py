2ddc2741c089794925642714423eb3025efc1b56
jina <jina.oh.dev@gmail.com>
2022-10-13 22:58:57 +0900

    2805

--- python/class2/Q2805.py
+++ python/class2/Q2805.py
@@ -2,14 +2,16 @@ import sys
 n,m =map(int,sys.stdin.readline().split())
 trees=list(map(int,sys.stdin.readline().split()))
 right = max(trees)
-left = 0
-while left < right :
+left = 1
+while left <= right :
     mid = (left + right ) //2
     total = 0
     for tree in trees:
         if tree > mid:
             total+= (tree-mid)
-    if total > m :
+        if total > m:
+            break
+    if total >= m :
         left = mid +1
     else :
         right = mid-1
